34eedd382eb3cbacc5bd167fce29dd1c30884644
Dino Spumony <aspumony@gmail.com>
2021-01-26 10:14:32 +0200

    Root for gh-pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,15 @@ import { NavLink } from 'react-router-dom'
 export const Navbar: React.FunctionComponent = () => (
   <nav>
     <div className="nav-wrapper indigo darken-1 p-5">
-      <a href="/" className="brand-logo">
+      <a href="/todo-ts" className="brand-logo">
         Todo with TS
       </a>
       <ul className="right hide-on-med-and-down">
         <li>
-          <NavLink to="/">Todo List</NavLink>
+          <NavLink to="/todo-ts">Todo List</NavLink>
         </li>
         <li>
-          <NavLink to="/about">Information</NavLink>
+          <NavLink to="/todo-ts/about">Information</NavLink>
         </li>
       </ul>
     </div>
